fb6f5ecbd67de885cfed2fa567dd6cdb006d9340
Polinikus <poluna1999@mail.ru>
2023-02-24 20:35:41 +0300

    2.1.2 Spring
    2.1.2 Spring

---
+++ src/main/java/App.java
@@ -0,0 +1,28 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+
+        Cat cat1 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(cat1.getMessage2());
+
+        Cat cat2 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(cat2.getMessage2());
+
+        System.out.println("Ссылки равны?" + " " + (bean == bean2));
+        System.out.println("Ссылки равны?" + " " + (cat1 == cat2));
+
+
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String message2;
+
+    public String getMessage2() {
+        return message2;
+    }
+
+    public void setMessage2(String message2) {
+        this.message2 = message2;
+    }
+}
